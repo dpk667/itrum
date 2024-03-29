26a72c1cc074846c713ff2762203990ca62dd1bd
dpk667 <pashadereza@gmail.com>
2024-03-29 10:16:03 +0300

    change task1

--- src/main/java/ru/dpk/concurrency/task1/BlockingQueue.java
+++ src/main/java/ru/dpk/concurrency/task1/BlockingQueue.java
@@ -3,7 +3,6 @@ package ru.dpk.concurrency.task1;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// use wait, notify
 public class BlockingQueue {
     private final Queue<Integer> queue;
     private final int size;
@@ -13,74 +12,71 @@ public class BlockingQueue {
         this.size = size;
     }
 
-    // add el to queue
-    public synchronized void enqueue(int i ) throws InterruptedException {
-
+    // Добавление элемента в очередь
+    public synchronized void enqueue(int i) throws InterruptedException {
         while (queue.size() == size) {
-            wait(); // пока очередь не освободится - ждем
+            wait(); // Ждем, пока очередь не освободится
         }
-        queue.offer(i); // добавляем элемент, убедившись, что не получим исключение
-        notifyAll(); // оповещаем все потоки, что добавился ээлемент
+        queue.offer(i); // Добавляем элемент в очередь
+        notifyAll(); // Оповещаем ожидающие потоки
     }
 
-    // for get el from queue
-    // if queue is empty, method will block thread before new element was not created
+    // Извлечение элемента из очереди
     public synchronized int dequeue() throws InterruptedException {
-        // если queue пустая, ждем новых элементов
         while (queue.isEmpty()) {
-            wait();
+            wait(); // Ждем, пока очередь не заполнится
         }
-
-        int i = queue.peek(); // извлекаем наш элемент, предупреждаем об этом все наши потоки
-        notifyAll();
+        int i = queue.poll(); // Извлекаем элемент из очереди
+        notifyAll(); // Оповещаем ожидающие потоки
         return i;
     }
 
-
+    // Возвращает текущий размер очереди
     public int size() {
         return queue.size();
     }
 
     public static void main(String[] args) {
-
         BlockingQueue queue = new BlockingQueue(5);
 
         Thread producer = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    for(int i = 0; i < 10; i++) {
+                    for (int i = 0; i < 10; i++) {
                         System.out.println("thread producer adds: " + i);
                         queue.enqueue(i);
                     }
+                    System.out.println("producer finished.");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
-    Thread consumer = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            try {
-                for (int i =0; i < 10; i++) {
-                    int point = queue.dequeue();
-                    System.out.println("thread consumed deleted: " + i);
 
+        Thread consumer = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        int point = queue.dequeue();
+                        System.out.println("thread consumer deleted: " + point);
+                    }
+                    System.out.println("consumer finished.");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
             }
+        });
+
+        producer.start();
+        consumer.start();
+
+        try {
+            producer.join();
+            consumer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    });
-
-    producer.start();
-    consumer.start();
-
-    try {
-        producer.join();
-        consumer.join();
-    } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-    }
     }
 }
